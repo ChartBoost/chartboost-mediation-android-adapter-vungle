013d3318b7ed4a6c1ecec1e41d606c108e0630ba
CB-RyanMcCormick <122290337+CB-RyanMcCormick@users.noreply.github.com>
2023-05-30 13:05:03 -0500

    Add proguard rules (#39)
    * Add proguard rules
    
    * Update docs.
    
    * Include rules in AAR

--- VungleAdapter/build.gradle.kts
+++ VungleAdapter/build.gradle.kts
@@ -35,8 +35,10 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.6.12.1.2"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.6.12.1.3"
         buildConfigField("String", "CHARTBOOST_MEDIATION_VUNGLE_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
+
+        consumerProguardFiles("proguard-rules.pro")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
